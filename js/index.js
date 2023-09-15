be7aa87ce0668219af3c8a3557dcff28b9879252
ppu3842 <ppu3842@naver.com>
2023-09-16 00:31:37 +0900

    컨테이너 부분 대락적인 요소 배치 완료 다음작업 =>서브 카테고리 작업

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const 패션의류= ["","","","","",""];
+const 브랜드패션= ["","","","","",""];
+const 디지털= ["","","","","",""];
+
+$('document').ready(function() {
+    var sub_categori= document.querySelectorAll('.sub_categori_in');
+    sub_categori[0].innerHTML='';
+})
