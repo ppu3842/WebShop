f83773da101fe44eb4db48cefd0d4a4255dc123c
hansolbi <hannsolbi@gmail.com>
2023-09-17 22:39:26 +0900

    메인 홈페이지 끝 이제 로그인 페이지 만들고 버튼이랑 연동해야됨

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function on_sub_costume1(){
+    $('.costumegoods1').show();
+    $('.foodgoods1').hide();
+}
+
+function on_sub_food1(){
+    $('.foodgoods1').show();
+    $('.cosyumegoods1').hide();
+}
